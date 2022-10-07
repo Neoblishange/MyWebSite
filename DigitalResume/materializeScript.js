f1f80efbb9073b9560cb31d559ca8898ec72691e
Florent SOR <74345944+Neoblishange@users.noreply.github.com>
2022-10-07 23:12:20 +0200

    "my web site, contact form not implemented"

---
+++ DigitalResume/materializeScript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel(
+        {
+            duration: 500,
+            noWrap: true,
+            padding: 20,
+            dist: -100,
+            shift:10,
+        });
+});
